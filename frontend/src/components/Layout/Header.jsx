c47d65451b5116272f91e09843c768645d7690e7
Aditya mishra <adityamishra6674.com>
2026-01-18 18:15:12 +0530

    nav-footer

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { NavLink, Link } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
 
-export default Header
+        {/* Brand Logo */}
+        <Link to="/" className="navbar-brand">
+          <FaCartShopping /> Ecommerce
+        </Link>
+
+        {/* Toggle Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu */}
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/register"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Register
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/login"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Login
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/cart"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Cart (0)
+              </NavLink>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
